823340685505d5026400e55aaa6b24d775ff75a9
luislf <luisloez89@gmail.com>
2021-09-06 22:27:04 +0200

    [project_3]Implement tests

--- project_3/project-6/truffle-config.js
+++ project_3/project-6/truffle-config.js
@@ -1,15 +1,28 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*" // Match any network id
-    }
+      network_id: "*", // Match any network id
+      websockets: true,
+      networkCheckTimeout: 999999
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/4243cd6e24404d90aa26be793fe0e8f0`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000,
+        networkCheckTimeout: 1000000,
+        timeoutBlocks: 200
+    },
   },
   compilers: {
     solc: {
-      version: "^0.4.24"
+      version: "0.5.0"
     }
   }
 };
