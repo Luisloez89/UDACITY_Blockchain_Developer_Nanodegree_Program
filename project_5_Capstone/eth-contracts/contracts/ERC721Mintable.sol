92fbaec68403bb10bf848b4417e48291703fcf47
luisloez89 <luisloez89@gmail.com>
2021-09-20 16:55:18 +0200

    [project_5]Implement HouseListingToken

--- project_5_Capstone/eth-contracts/contracts/ERC721Mintable.sol
+++ project_5_Capstone/eth-contracts/contracts/ERC721Mintable.sol
@@ -585,6 +585,13 @@ contract ERC721Metadata is ERC721Enumerable, usingOraclize {
 //      -takes in a 'to' address, tokenId, and tokenURI as parameters
 //      -returns a true boolean upon completion of the function
 //      -calls the superclass mint and setTokenURI functions
+contract HouseListingToken is ERC721Metadata("House Listing Token", "HLT", "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/") {
 
+    function mint(address to, uint256 tokenId) public onlyOwner returns(bool) {
+        super._mint(to, tokenId);
+        super.setTokenURI(tokenId);
+        return true;
+    }
+}
 
 
